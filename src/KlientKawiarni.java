5113df1471146a10bcd973830caafbc24a35d471
dantech13 <daniil.mur2004@gmail.com>
2026-04-01 23:51:29 +0200

    .

---
+++ src/KlientKawiarni.java
@@ -0,0 +1,37 @@
+
+public class KlientKawiarni {
+        private int idKlienta;
+        private String imie;
+        private String nazwisko;
+        private String email;
+
+        public KlientKawiarni(int idKlienta, String imie, String nazwisko, String email) {
+            this.idKlienta = idKlienta;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.email = email;
+        }
+
+        public String pelneDane() {
+            return imie + " " + nazwisko + " (" + email + ")";
+        }
+
+        @Override
+        public String toString() {
+            return "KlientKawiarni{" +
+                    "idKlienta=" + idKlienta +
+                    ", imie='" + imie + '\'' +
+                    ", nazwisko='" + nazwisko + '\'' +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof KlientKawiarni)) return false;
+
+            KlientKawiarni other = (KlientKawiarni) obj;
+            return this.email.equals(other.email);
+        }
+    }
